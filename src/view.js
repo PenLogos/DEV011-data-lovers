1a3c5e4ae172998dda87b06b00972ba984533dae
PenLogos <penelopelogos@gmail.com>
2023-09-19 09:39:13 -0500

    se terminan de llenar las tarjetas con los datos restantes, queda pendiente la tercera condición de las evoluciones para pokemones adultos

--- src/view.js
+++ src/view.js
@@ -9,16 +9,18 @@ export const renderItems = (pokemon) => {
   li.setAttribute("itemscope", "");
   li.setAttribute("itemtype", "pokemon");
 
-  const pokemonImage = document.createElement("img");
-  li.appendChild(pokemonImage);
-  pokemonImage.setAttribute("itemprop", "img-pokemon");
-  pokemonImage.src = pokemon.img;
-
+  console.log(pokemon)
+  
+  // const pokemonImage = document.createElement("img");
+  // li.appendChild(pokemonImage);
+  // pokemonImage.setAttribute("itemprop", "img-pokemon");
+  // pokemonImage.src = pokemon.img;
+  
   const pokemonName = document.createElement("dd");
   li.appendChild(pokemonName);
   pokemonName.setAttribute("itemprop", "name");
   pokemonName.innerHTML = pokemon.name;
-
+  
   const pokemonType = document.createElement("dd");
   li.appendChild(pokemonType);
   pokemonType.setAttribute("itemprop", "type");
@@ -44,12 +46,10 @@ export const renderItems = (pokemon) => {
   li.appendChild(evolutionImage1);
   evolutionImage1.setAttribute("itemprop", "img-evolution");
 
-
   const evolution1 = document.createElement("dd");
   li.appendChild(evolution1);
   evolution1.setAttribute("itemprop", "evoluciones");
   if (pokemon.evolution['next-evolution'][0]['next-evolution']) {
-    console.log('se cumple la condición')
     evolution1.innerHTML = 'evolución 1: ' + pokemon.evolution['next-evolution'][0].name
   }
   else if (pokemon.evolution['next-evolution'] && pokemon.evolution['prev-evolution']) {
@@ -58,11 +58,18 @@ export const renderItems = (pokemon) => {
   else if (pokemon.evolution['prev-evolution'][0]['prev-evolution']) {
     evolution1.innerHTML = 'preevolución 1 ' + pokemon.evolution['prev-evolution'][0]['prev-evolution'][0].name
   }
+  
+  const pokemonCandycost1 = document.createElement("dd");
+  li.appendChild(pokemonCandycost1);
+  pokemonCandycost1.setAttribute("itemprop", "candy-cost");
+  if (pokemon.evolution['next-evolution'][0]['next-evolution']) {
+    pokemonCandycost1.innerHTML = 'x' + pokemon.evolution['next-evolution'][0]['candy-cost']
+  }
 
   const evolutionImage2 = document.createElement("img");
   li.appendChild(evolutionImage2);
   evolutionImage2.setAttribute("itemprop", "img-evolution");
-
+  
   const evolution2 = document.createElement("dd");
   li.appendChild(evolution2);
   evolution2.setAttribute("itemprop", "evoluciones");
@@ -73,18 +80,19 @@ export const renderItems = (pokemon) => {
     evolution2.innerHTML = 'evolución: ' + pokemon.evolution['next-evolution'][0].name
   }
   else if (pokemon.evolution['prev-evolution'][0]['prev-evolution']) {
+    console.log('se cumple la condición')
     evolution2.innerHTML = 'preevolución 2: ' + pokemon.evolution['prev-evolution'][0].name
   }
 
-
-  const pokemonCandycost = document.createElement("dd");
-  li.appendChild(pokemonCandycost);
-  pokemonCandycost.setAttribute("itemprop", "candy-cost");
-
-  const spawnChance = document.createElement("dt");
-  li.appendChild(spawnChance);
+  const pokemonCandycost2 = document.createElement("dd");
+  li.appendChild(pokemonCandycost2);
+  pokemonCandycost2.setAttribute("itemprop", "candy-cost");
+  if (pokemon.evolution['prev-evolution'] && pokemon.evolution['next-evolution']) {
+    pokemonCandycost2.innerHTML = 'x' + pokemon.evolution['next-evolution'][0]['candy-cost']
+  }
 
   const pokemonSpawnChance = document.createElement("dd");
   li.appendChild(pokemonSpawnChance);
   pokemonSpawnChance.setAttribute("itemprop", "spawn-chance");
+  pokemonSpawnChance.innerHTML = (`Tasa de aparición: ${pokemon['spawn-chance']}`)
 };
