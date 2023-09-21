7e6fdcf10a02ec7bd5254505dbc0476b69b75946
PenLogos <penelopelogos@gmail.com>
2023-09-21 18:17:30 -0500

    cambios

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,7 +3,7 @@
 export const typeFilter = (data, filterBy, value) => {
   //const optionPokemonTypeSelected = data.filter(type => type === type.grass); 
   const filter = data.filter((element)=>{
-    return element[filterBy] === value
+    return element[filterBy].includes === value
   })
   return filter;
   // console.log("Hola");
--- src/main.js
+++ src/main.js
@@ -5,9 +5,40 @@ import data from "./data/pokemon/pokemon.js";
 
 console.log(data.pokemon)
 const selectPokemon = data.pokemon;
-const selectFilterType = document.querySelector("select[name=type]");
-const selectFilterResistant = document.querySelector("select[name=resistant-to]");
-const selectFilterWeakness = document.querySelector("select[name=weak-to]");
+const main = document.querySelector(".filters");
+
+const labelFilterType = document.createElement("label");
+main.appendChild(labelFilterType);
+labelFilterType.setAttribute("class", "filter");
+labelFilterType.setAttribute("for", "type");
+labelFilterType.innerHTML = "Tipo";
+
+const selectType = document.createElement("select");
+labelFilterType.appendChild(selectType);
+labelFilterType.setAttribute("name", "type");
+labelFilterType.setAttribute("data-testid", "select-filter");
+
+const labelFilterResistant = document.createElement("label");
+main.appendChild(labelFilterResistant);
+labelFilterResistant.setAttribute("class", "filter");
+labelFilterResistant.setAttribute("for", "resistant-to");
+labelFilterResistant.innerHTML = "Resiste a";
+
+const selectResistant = document.createElement("select");
+labelFilterResistant.appendChild(selectResistant);
+labelFilterResistant.setAttribute("name", "resistant-to");
+labelFilterResistant.setAttribute("data-testid", "select-filter");
+
+const labelFilterWeakness = document.createElement("label");
+main.appendChild(labelFilterWeakness);
+labelFilterWeakness.setAttribute("class", "filter");
+labelFilterWeakness.setAttribute("for", "weak-to");
+labelFilterWeakness.innerHTML = "Débil a";
+
+const selectWeakness = document.createElement("select");
+labelFilterWeakness.appendChild(selectWeakness );
+labelFilterWeakness.setAttribute("name", "weak-to");
+labelFilterWeakness.setAttribute("data-testid", "select-filter");
 
 const arrayTypeValues = Object.values(selectPokemon).flatMap(
   (item) => item.type
@@ -16,29 +47,23 @@ const cleanArrayTypeValues = [...new Set(arrayTypeValues)];
 
 function createOption(value) {
   const optionType = document.createElement("option");
-  selectFilterType.appendChild(optionType);
+  selectType.appendChild(optionType);
   optionType.innerHTML = value;
   optionType.setAttribute("class", "type-filter");
-  optionType.setAttribute("value", optionType.value);
   const optionResistant = document.createElement("option");
-  selectFilterResistant.appendChild(optionResistant );
+  selectResistant.appendChild(optionResistant );
   optionResistant.innerHTML = value;
-  optionResistant.setAttribute("value", optionResistant.value);
   const optionWeakness = document.createElement("option");
-  selectFilterWeakness.appendChild(optionWeakness);
+  selectWeakness.appendChild(optionWeakness);
   optionWeakness.innerHTML = value;
-  optionWeakness.setAttribute("value", optionWeakness.value);
 }
 
 cleanArrayTypeValues.forEach(createOption);
 
 selectPokemon.forEach(renderItems);
 
-selectFilterType.addEventListener("change", ()=> { 
-  // console.log(selectFilterType.value);
-  // console.log(selectPokemon);
-  // console.log(data.pokemon[0].type);
-  const resultFilter = typeFilter(selectPokemon, "type", selectFilterType.value)
-  console.log(resultFilter);
-});
+// const selectTypeOption = document.querySelectorAll(".type-filter");
+// selectTypeOption.forEach(option => {option.addEventListener("click", typeFilter() {
+//    console.log("Hola")
+// })});
 
